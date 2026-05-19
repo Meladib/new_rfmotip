78e0f41a3b335c8321eacfdc80717fca9921281a
moad eladib <144381364+Meladib@users.noreply.github.com>
2026-05-19 16:04:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 \
 accelerate launch --num_processes=4 train.py \
     --data-root /data/pos+mot/Datadir/ \
     --exp-name rfmotip_dancetrack \
-    --config-path ./configs/rf_detr_motip_dancetrack.yaml
+    --config-path ./configs/rf_detrV4_motip_dancetrack.yaml
